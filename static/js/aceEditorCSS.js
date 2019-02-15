69a6bb3253f5abbd0891b745ae9d6dcd1afcf441
Luiza Pagliari <lpagliari@gmail.com>
2019-02-15 09:32:57 -0200

    [fix] Create plugin part only for `disable_on_paste.css` (#28)
    There are some styles that need to be disabled on paste of large
    contents into Etherpad. Those styles cannot be bundled with the other
    CSS files because there won't be any way to remove them during the paste
    processing, so they need to be available on separated files on Etherpad.
    
    Implement part of https://trello.com/c/aSCjkNwv/1716.

--- static/js/aceEditorCSS.js
+++ static/js/aceEditorCSS.js
@@ -1,7 +1,13 @@
 exports.CSS_TO_BE_DISABLED_ON_PASTE = 'ep_script_elements/static/css/disable_on_paste.css';
+
 exports.aceEditorCSS = function() {
   return [
     'ep_script_elements/static/css/editor.css',
+  ];
+}
+
+exports.disableOnPaste = function() {
+  return [
     exports.CSS_TO_BE_DISABLED_ON_PASTE,
   ];
 }
